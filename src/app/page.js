a03820573b40eade148278fa152e1c69059056e6
mostafizur rahman <dev.mostafizurrahman@gmail.com>
2023-11-06 16:48:59 +0600

    admin dashboard

--- src/app/page.js
+++ src/app/page.js
@@ -134,7 +134,7 @@ export default function Home() {
                   className="drawer-overlay"
                 ></label>
                 <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
-                  {/* Sidebar content here */}
+                 
                   <li>
                     <a>Dashboard</a>
                   </li>
